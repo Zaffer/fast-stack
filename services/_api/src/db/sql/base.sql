9c1d0c72e6e81e792c3ae8ad4570a5cc03123d5c
Zaffer <jameswiles@gmail.com>
2023-01-10 22:00:02 +0200

    alembic updates

--- services/_api/src/db/sql/base.sql
+++ services/_api/src/db/sql/base.sql
@@ -15,5 +15,19 @@ CREATE TABLE "user" (
 
 INSERT INTO alembic_version (version_num) VALUES ('e6024baad4be') RETURNING alembic_version.version_num;
 
+-- Running upgrade e6024baad4be -> 54738a63b50d
+
+ALTER TABLE "user" ADD COLUMN email VARCHAR NOT NULL;
+
+ALTER TABLE "user" ALTER COLUMN name DROP NOT NULL;
+
+ALTER TABLE "user" ADD UNIQUE (email);
+
+UPDATE alembic_version SET version_num='54738a63b50d' WHERE alembic_version.version_num = 'e6024baad4be';
+
+-- Running upgrade 54738a63b50d -> d6a8dc21c57d
+
+UPDATE alembic_version SET version_num='d6a8dc21c57d' WHERE alembic_version.version_num = '54738a63b50d';
+
 COMMIT;
 
