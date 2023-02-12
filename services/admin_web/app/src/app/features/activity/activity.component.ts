ea809ff9c34ec079f1ab2690c12c96a56b2a43ed
Zaffer <jameswiles@gmail.com>
2023-02-12 13:47:26 +0200

    card plot styling

--- services/admin_web/app/src/app/features/activity/activity.component.ts
+++ services/admin_web/app/src/app/features/activity/activity.component.ts
@@ -12,7 +12,9 @@ export class ActivityComponent {
       { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: { color: 'red' } },
       { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
     ],
-    layout: { width: 320, height: 240, title: 'A Fancy Plot' }
+    layout: {
+      margin: { t: 20, r: 20, b: 20, l: 20 },
+      autosize: true,
+    },
   };
-
 }
