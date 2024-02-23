7d87222095e1867a094deb1fbff9dfc262b87315
Zaffer <jameswiles@gmail.com>
2024-02-23 16:54:51 -0500

    Add Google Fonts and Material Icons*** ***Update app.config.ts*** ***Update package.json*** ***Add Material Theming to styles.scss

---
+++ services/_templates/web_admin/svc/src/app/features/dash/dash.component.spec.ts
@@ -0,0 +1,25 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { DashComponent } from './dash.component';
+
+describe('DashComponent', () => {
+  let component: DashComponent;
+  let fixture: ComponentFixture<DashComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [NoopAnimationsModule]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DashComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
